b26de45ab13056f7c3928ed485eb684764447a4e
JDanielChaconA <danielakadanielsan506@gmail.com>
2023-09-24 12:44:58 -0600

    Script de la Base de datos Actualizado

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 // Configura CORS para permitir todas las solicitudes
 app.use(cors());
@@ -9,23 +9,23 @@ app.use(cors());
 // Resto de tu configuración de servidor aquí...
 
 //archivos estáticos
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Definir rutas
-const routes = require('./routes/routes.js');
-app.use('/api', routes);
+const routes = require("./routes/routes.js");
+app.use("/api", routes);
 
-const clienteRoutes = require('./routes/clienteRouter.js');
-app.use('/clientes', clienteRoutes);
+const clienteRoutes = require("./routes/clienteRouter.js");
+app.use("/clientes", clienteRoutes);
 
-const eventoRoutes = require('./routes/eventoRouter.js');
-app.use('/eventos', eventoRoutes);
+const eventoRoutes = require("./routes/eventoRouter.js");
+app.use("/eventos", eventoRoutes);
 
-const usuarioRoutes = require('./routes/usuarioRouter.js');
-app.use('/usuarios', usuarioRoutes);
+const usuarioRoutes = require("./routes/usuarioRouter.js");
+app.use("/usuarios", usuarioRoutes);
 
 //configurar puerto
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 //ponerlo a escuchar
 app.listen(PORT, () => {
