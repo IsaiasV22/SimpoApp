a61848ae334154a05d2f755edcdc8d817b243f76
andresF <andres.fallas.alvarado@est.una.ac.cr>
2023-09-24 22:32:45 -0600

    mostrar  los simposios
    se muestran los simposios en la paga Simposio.jsx por medio de cards

--- Frontend/src/app/components/simposios/Simposios.jsx
+++ Frontend/src/app/components/simposios/Simposios.jsx
@@ -4,6 +4,8 @@ import "./Simposios.css";
 import Link from "next/link";
 import { urlServer } from "@/app/Utiles.jsx";
 
+const urlSimposio = "../simposio";
+
 // FIXME: No se muestra la lista de eventos
 export default function Simposios() {
   const [eventos, setEventos] = useState([]);
@@ -21,34 +23,40 @@ export default function Simposios() {
 
       const data = await response.json();
       setEventos(data.eventos);
-      console.log(eventos);
     } catch (error) {
       alert(error.message);
     }
   }
 
   return (
-    <div className="main-content">
+    <div className="container my-5">
       <Link href="../login">
-        <button>ATRAS</button>
+        <button className="btn btn-primary mb-3">ATRAS</button>
       </Link>
-      <div className="simposios-container">
-        <h1>Simposios</h1>
 
+      <h1 className="mb-4">Simposios</h1>
 
+      <div className="row">
         {eventos.length > 0 ? (
-          eventos.map((evento) => (
-            <div key={evento.id}>Eventos: {
-              eventos.forEach(element => {
-                console.log(element);
-                <div>
-                  <h1>{element.nombre}</h1>
+          eventos.map((element) => (
+            <div key={element.evento.id} className="col-md-4 mb-4">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{element.evento.nombre}</h5>
+                  <p className="card-text">{element.evento.descripcion}</p>
+                  <p className="card-text">{element.evento.fecha}</p>
+                  <p className="card-text">{element.evento.hora}</p>
+                  <p className="card-text">{element.evento.lugar}</p>
+                  <p className="card-text">{element.evento.costo}</p>
+                  <Link href={`${urlSimposio}`}>
+                    <button className="btn btn-primary">Ver más</button>
+                  </Link>
                 </div>
-              })
-            }</div>
+              </div>
+            </div>
           ))
         ) : (
-          <div>No hay eventos próximos.</div>
+          <div className="col-12">No hay eventos próximos.</div>
         )}
       </div>
     </div>
