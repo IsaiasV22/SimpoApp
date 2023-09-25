31d10b6f3310ad7a645413f8e0f37a35b99b5a4b
andresF <andres.fallas.alvarado@est.una.ac.cr>
2023-09-24 22:42:25 -0600

    update de los cards de los simposios

--- Frontend/src/app/components/simposios/Simposios.jsx
+++ Frontend/src/app/components/simposios/Simposios.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import "./Simposios.css";
 import Link from "next/link";
 import { urlServer } from "@/app/Utiles.jsx";
+//import "@/app/App.css"
 
 const urlSimposio = "../simposio";
 
@@ -29,35 +30,37 @@ export default function Simposios() {
   }
 
   return (
-    <div className="container my-5">
-      <Link href="../login">
-        <button className="btn btn-primary mb-3">ATRAS</button>
-      </Link>
+    <div className="main-content">
+      <div className="container my-5">
+        <Link href="../login">
+          <button className="btn btn-primary mb-3">ATRAS</button>
+        </Link>
 
-      <h1 className="mb-4">Simposios</h1>
+        <h1 className="mb-4">Simposios</h1>
 
-      <div className="row">
-        {eventos.length > 0 ? (
-          eventos.map((element) => (
-            <div key={element.evento.id} className="col-md-4 mb-4">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">{element.evento.nombre}</h5>
-                  <p className="card-text">{element.evento.descripcion}</p>
-                  <p className="card-text">{element.evento.fecha}</p>
-                  <p className="card-text">{element.evento.hora}</p>
-                  <p className="card-text">{element.evento.lugar}</p>
-                  <p className="card-text">{element.evento.costo}</p>
-                  <Link href={`${urlSimposio}`}>
-                    <button className="btn btn-primary">Ver más</button>
-                  </Link>
+        <div className="row">
+          {eventos.length > 0 ? (
+            eventos.map((element) => (
+              <div key={element.evento.id} className="col-12 mb-4">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{element.evento.nombre}</h5>
+                    <p className="card-text">{element.evento.descripcion}</p>
+                    <p className="card-text">{element.evento.fecha}</p>
+                    <p className="card-text">{element.evento.hora}</p>
+                    <p className="card-text">{element.evento.lugar}</p>
+                    <p className="card-text">{element.evento.costo}</p>
+                    <Link href={`${urlSimposio}`}>
+                      <button className="btn btn-primary">Ver más</button>
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <div className="col-12">No hay eventos próximos.</div>
-        )}
+            ))
+          ) : (
+            <div className="col-12">No hay eventos próximos.</div>
+          )}
+        </div>
       </div>
     </div>
   );
