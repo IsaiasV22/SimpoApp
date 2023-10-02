b455b4321edef49afcb66c3dd4709f2a244725c3
andresF <andres.fallas.alvarado@est.una.ac.cr>
2023-10-01 20:12:03 -0600

    error de warnings arreglas

--- Frontend/src/app/simposio/page.jsx
+++ Frontend/src/app/simposio/page.jsx
@@ -1,27 +1,21 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Simposio from "../components/simposio/Simposio";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import { useSearchParams } from "next/navigation";
 
 const Page = () => {
-  //const [element, setElement] = useState(null);
   const searchParams = useSearchParams();
-
-
-    const elementValues = searchParams.get("element");
-    console.log("Elemto del searchParams: ",elementValues);
-
-      //setElement(JSON.parse(elementValues));
-      //console.log("Elemento del estado: ",element);
-
+  const elementValues = searchParams.get("element");
+  const element = elementValues ? JSON.parse(elementValues) : null;
+  console.log("Elemento: ",element);
 
   return (
     <>
       <Header />
       <div className="main-content">
-        <Simposio element={JSON.parse(elementValues)} />
+        <Simposio element={element} />
       </div>
       <Footer />
     </>
