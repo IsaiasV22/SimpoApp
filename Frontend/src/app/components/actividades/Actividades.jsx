2cccb61e554c28248a76b017b0b33303069c78af
yiri275 <yiriana.guevara.araya@est.una.ac.cr>
2023-10-01 20:12:06 -0600

    Warning Actividades

--- Frontend/src/app/components/actividades/Actividades.jsx
+++ Frontend/src/app/components/actividades/Actividades.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import "./Actividades.css"; // Asegúrate de que la ruta al archivo CSS sea correcta
 import { urlServer } from "@/app/Utiles.jsx";
-//import Link
+// Importa Link desde Next.js
 import Link from "next/link";
 
 const urlSimposio = "../simposio";
@@ -10,6 +10,7 @@ const urlActividad = "../../actividad";
 
 export default function Actividades() {
   const [actividades, setActividades] = useState([]);
+
   useEffect(() => {
     handleActividades();
   }, []);
@@ -34,21 +35,20 @@ export default function Actividades() {
   return (
     <div className="main-content">
       <div className="container my-5">
-
         <h1 className="mb-4">Actividades</h1>
-
         <div className="row">
           {actividades.length > 0 ? (
             actividades.map((element) => (
-              <div key={element.id} className="col-12 mb-4">
+              <div key={element.PK_actividad} className="col-12 mb-4">
+                {/* Utiliza el campo PK_actividad como clave única */}
                 <div className="card">
                   <div className="card-body">
                     <h5 className="card-title">{element.descripcion}</h5>
-                    <p className="card-text">{"Fecha: "+element.dia_evento.slice(0, 10)}</p>
-                    <p className="card-text">{"Hora Inicio: "+element.hora_inicio}</p>
-                    <p className="card-text">{"Hora Final: "+element.hora_final}</p>
-                    <p className="card-text">{"Ubicacion: "+element.ubicacion}</p>
-                    <p className="card-text">{"Estatus: "+element.estatus}</p>
+                    <p className="card-text">{"Fecha: " + element.dia_evento.slice(0, 10)}</p>
+                    <p className="card-text">{"Hora Inicio: " + element.hora_inicio}</p>
+                    <p className="card-text">{"Hora Final: " + element.hora_final}</p>
+                    <p className="card-text">{"Ubicación: " + element.ubicacion}</p>
+                    <p className="card-text">{"Estatus: " + element.estatus}</p>
                     <Link href={`${urlActividad}`}>
                       <button className="btn btn-primary">Ver más</button>
                     </Link>
