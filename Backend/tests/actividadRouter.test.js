02340550558f1526351ccc81289a4c85c5de2982
IsaiasViquez <viquez.isaias@gmail.com>
2023-10-21 18:35:06 -0600

    Test update actividad

--- Backend/tests/actividadRouter.test.js
+++ Backend/tests/actividadRouter.test.js
@@ -34,6 +34,28 @@ describe("Pruebas para el enrutador de actividades", () => {
     ]);
   });
 
+  it("debe responder correctamente a una solicitud PUT /actividades/updateActivity", async () => {
+    const actividadActualizada = {
+      PK_actividad: 1,
+      descripcion: "A nonlinear relapse model with disaggregated contact rates: analysis of a forward-backward bifurcation",
+      descripcion_d: "Throughout the progress of epidemic scenarios it is expected to have different average daily contact behavior for individuals that are at different health classes. This contact heterogeneity has been studied in recent adaptive models and it allows to better captures the inherent differences across health statuses. Diseases with reinfection bring out more complex scenarios and they offer an important application in which to consider contact disaggregation. Therefore, we developed a nonlinear differential equation model to explore the dynamics of relapse phenomena and contact differences across health statuses. Our incidence rate function is formulated, taking inspiration from recent adaptive algorithms. It incorporates contact behavior for individuals in each health class. We use constant contact rates at each health status for our analytical results and prove conditions for different forward-backward bifurcation scenarios. The relationship between the different contact rates heavily influences these conditions. Numerical examples highlight the effect of temporarily recovered individuals and initial conditions on infected population persistence.",
+      dia_evento: "2018-02-27",
+      hora_inicio: "14:00:00",
+      hora_final: "16:00:00",
+      ubicacion: "Salón principal",
+      estatus: 1,
+      FK_tema: 1,
+      FK_taller: 1,
+      FK_evento_contenedor: 1
+    };
+
+    const response = await supertest(app)
+      .put("/actividades/updateActivity")
+      .send(actividadActualizada);
+
+    expect(response.status).toBe(204);
+  });
+
   //Prueba para obtener una actividad con un id que no existe
   it("debe responder con un error a una solicitud POST /actividades/porId si el id no existe", async () => {
     const response = await supertest(app)
