c2a2f1a5b06f012c949312e981177e9fd4d132a1
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2023-10-23 03:16:58 -0600

    notification in PWA

--- Frontend/src/app/page.jsx
+++ Frontend/src/app/page.jsx
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Login from "@/app/components/login/Login";
 
+
 export default function Home() {
   return (
     <div>
       <div className="main-content">
         <Login />
+
       </div>
     </div>
   );
