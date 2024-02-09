86476805ed69475822bb066ec09cdf98bd8633b3
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2024-02-09 12:36:01 -0600

    Pagination with styles

--- Frontend/src/app/components/actividades/Pagination.jsx
+++ Frontend/src/app/components/actividades/Pagination.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import  Pagination  from "react-bootstrap/Pagination";
+import Pagination from "react-bootstrap/Pagination";
+import './Pagination.css';
 
-const pagination = ({ Actividades, actividadesPerPage,setCurrentPage,currentPage, setActividadesPerPage }) => {
-  console.log(currentPage)
+const pagination = ({
+  Actividades,
+  actividadesPerPage,
+  setCurrentPage,
+  currentPage,
+  setActividadesPerPage,
+}) => {
+  console.log(currentPage);
   let pages = [];
-  for (let i = 1; i <= Math.ceil(Actividades.length / actividadesPerPage); i++) {
+  for (
+    let i = 1;
+    i <= Math.ceil(Actividades.length / actividadesPerPage);
+    i++
+  ) {
     pages.push(i);
   }
 
@@ -18,13 +29,11 @@ const pagination = ({ Actividades, actividadesPerPage,setCurrentPage,currentPage
   };
 
   const handlePrevPage = () => {
-    if(currentPage > 1)
-    setCurrentPage(currentPage - 1);
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
   };
 
   const handleNextPage = () => {
-    if(currentPage < pages.length)
-    setCurrentPage(currentPage + 1);
+    if (currentPage < pages.length) setCurrentPage(currentPage + 1);
   };
 
   const handleLastPage = () => {
@@ -37,27 +46,47 @@ const pagination = ({ Actividades, actividadesPerPage,setCurrentPage,currentPage
   };
   return (
     <div>
-        <br/>
-        <span>Activities per page: </span>
+      <hr/>
+      <div className="centered">
+        <br />
+        <div style={{'display':'flex','marginBottom':'2px'}}>
+        <span className='m-1 '>Activities per page:</span>
         <select onChange={handleActivitiesPerPage} value={actividadesPerPage}>
-        {Array.from({ length: Actividades.length }, (_, i) => (
-          <option key={i + 1} value={i + 1}>
-            {i + 1}
-          </option>
-        ))}
+          {Array.from({ length: Actividades.length }, (_, i) => (
+            <option key={i + 1} value={i + 1}>
+              {i + 1}
+            </option>
+          ))}
         </select>
+        </div>
         <Pagination>
-            <Pagination.First title="Go back to First page" onClick={()=>handleFirstPage()}/>
-            <Pagination.Prev title="Go to previous page" onClick={()=>handlePrevPage()}/>
-            {pages.map((number) => (
-            <Pagination.Item key={number} active={number ===currentPage} onClick={()=>handleClick(number)}>
-                {number}
+          <Pagination.First
+            title="Go back to First page"
+            onClick={() => handleFirstPage()}
+          />
+          <Pagination.Prev
+            title="Go to previous page"
+            onClick={() => handlePrevPage()}
+          />
+          {pages.map((number) => (
+            <Pagination.Item
+              key={number}
+              active={number === currentPage}
+              onClick={() => handleClick(number)}
+            >
+              {number}
             </Pagination.Item>
-            ))}
-            <Pagination.Next title="Go to Next page" onClick={()=>handleNextPage()}/>
-            <Pagination.Last title="Go to Last page" onClick={()=>handleLastPage()}/>
+          ))}
+          <Pagination.Next
+            title="Go to Next page"
+            onClick={() => handleNextPage()}
+          />
+          <Pagination.Last
+            title="Go to Last page"
+            onClick={() => handleLastPage()}
+          />
         </Pagination>
-
+      </div>
     </div>
   );
 };
