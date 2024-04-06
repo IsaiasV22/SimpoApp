91e271e9d5cf3dcb918f00ac261f42810ec86dbd
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2024-04-06 13:31:21 -0600

    Back to Localhost

--- Frontend/src/app/Utiles.jsx
+++ Frontend/src/app/Utiles.jsx
@@ -1,2 +1,2 @@
-//export const urlServer="http://20.150.204.32:3001/"
-export const urlServer="https://simpo-app-official-backend-p7apldfbh-andres-projects-49880e0d.vercel.app/";
+export const urlServer="http://localhost:3001/"
+//export const urlServer="https://simpo-app-official-backend-p7apldfbh-andres-projects-49880e0d.vercel.app/";
