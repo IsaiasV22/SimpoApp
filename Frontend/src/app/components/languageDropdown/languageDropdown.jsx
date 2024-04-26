c5b0a6a9c1a817c2f950b04f3e04c5a5e049ce78
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2024-04-25 23:45:03 -0600

    Funcione pero al reves

--- Frontend/src/app/components/languageDropdown/languageDropdown.jsx
+++ Frontend/src/app/components/languageDropdown/languageDropdown.jsx
@@ -3,9 +3,22 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { useState } from "react";
 import useGlobalState from "../globalState/GlobalState";
 import Form from "react-bootstrap/Form";
+//import the i18n instance
+import "@/app/locales/locale";
+import { useTranslation } from "react-i18next";
+
 
 export default function languageDropdown() {
-  const [selectedLanguage, setSelectedLanguage] = useState("bandera1"); // Estado para almacenar la opción seleccionada
+  const [selectedLanguage, setSelectedLanguage] = useState("es"); // Estado para almacenar la opción seleccionada
+  //i18n
+  //i18n translate strategy
+  const { t, i18n } = useTranslation(["common"]);
+  // set selected language
+    const handleClick = (language) => {
+        console.log("language", language);
+        setSelectedLanguage(language);
+        i18n.changeLanguage(selectedLanguage);
+    };
 
   return (
     <NavDropdown.Item>
@@ -19,14 +32,14 @@ export default function languageDropdown() {
           {/* Engish en 1 */}
           <img
             src="https://cdn.gtranslate.net/flags/svg/en.svg"
-            onClick={() => setSelectedLanguage("en")}
-            style={{ width: "30px", cursor: "pointer", marginRight: "10px",}}
+            onClick={() => handleClick("en")}
+            style={{ width: "30px", cursor: "pointer", marginRight: "10px" }}
           />
           {/* Spanish es */}
           <img
             src="https://cdn.gtranslate.net/flags/svg/es.svg"
-            onClick={() => setSelectedLanguage("es")}
-            style={{ width: "30px", cursor: "pointer" ,marginRight: "10px",}}
+            onClick={() => handleClick("es")}
+            style={{ width: "30px", cursor: "pointer", marginRight: "10px" }}
           />
         </div>
       </div>
