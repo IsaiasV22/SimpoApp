571f48efb1de56a84237abce31ce70ea59ea2449
IsaiasViquez <viquez.isaias@gmail.com>
2024-04-29 11:56:17 -0600

    Functionality done

--- Frontend/src/app/components/ayuda/Ayuda.jsx
+++ Frontend/src/app/components/ayuda/Ayuda.jsx
@@ -1,50 +1,102 @@
 "use client";
 import React, { useState } from "react";
-//import "./Login.css";
 import { urlServer } from "@/app/Utiles";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useGlobalState from "../globalState/GlobalState";
-import { useRouter } from "next/navigation";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
-import { left } from "@popperjs/core";
+import { FormControl } from "react-bootstrap";
 
 export default function Ayuda() {
   const high_contrast = useGlobalState((state) => state.high_contrast);
+  const [nombre_usuario, setNombreUsuario] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [validated, setValidated] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+    }
+    setValidated(true);
+    const newSolicitudAyuda = {
+      nombre_usuario,
+      correo,
+      descripcion,
+    };
+    try {
+      const response = await fetch(`${urlServer}solicitudesAyuda/add`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newSolicitudAyuda),
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        console.log("response: ", response);
+        throw new Error(response.statusText);
+      }
+
+      toast.success("Solicitud enviada correctamente");
+      setTimeout(() => {
+        window.location.reload();
+      }, 1500);
+    } catch (error) {
+      console.log("error -> ", error);
+      toast.error(error.message);
+    }
+  }
 
   return (
     <div className={`main-content ${high_contrast ? "high-contrast" : ""}`}>
+      <ToastContainer />
       <div className="container my-5 ">
-        <div className="card" style={{ padding: "3%", margin: "10%" }}>
-          <Form id="editActividad">
-            <InputGroup className="mb-3">
+        <h1 className="mb-4" style={{ textAlign: "center" }}>
+          Solicitud de ayuda
+        </h1>
+        <div className="card" style={{ padding: "3%" }}>
+          <Form id="solicitudAyuda" noValidate validated={validated} onSubmit={handleSubmit}>
+            <InputGroup className="mb-2" has-validation >
               <Form.Control
-                placeholder="Escribe tu nombre"
-                aria-label=""
-                //value=""
-                //onChange={(e) => setDescripcion(e.target.value)}
+                type="text"
+                required
+                placeholder="Nombre"
+                value={nombre_usuario}
+                onChange={(e) => setNombreUsuario(e.target.value)}
               />
+              <FormControl.Feedback type="invalid">
+                Por favor ingrese su nombre
+              </FormControl.Feedback>
             </InputGroup>
-
-            <InputGroup className="mb-3">
+            <InputGroup className="mb-2" has-validation >
               <Form.Control
+                type="email"
+                required
                 placeholder="Correo"
-                aria-label=""
-                //value=""
-                //onChange={(e) => setUbicacion(e.target.value)}
+                value={correo}
+                onChange={(e) => setCorreo(e.target.value)}
               />
+              <FormControl.Feedback type="invalid">
+                Por favor ingrese un correo valido
+              </FormControl.Feedback>
             </InputGroup>
-            <InputGroup className="mb-3">
+            <InputGroup className="mb-2" has-validation >
               <Form.Control
+                type="text"
+                required
                 placeholder="Descripcion"
                 as="textarea"
-                aria-label=""
                 style={{ height: "150px" }}
-                //value=""
-                //onChange={(e) => setUbicacion(e.target.value)}
+                value={descripcion}
+                onChange={(e) => setDescripcion(e.target.value)}
               />
+              <FormControl.Feedback type="invalid">
+                Por favor ingrese una descripcion de su problema
+              </FormControl.Feedback>
             </InputGroup>
             <div className="d-flex justify-content-end">
               <Button variant="secondary" onClick={""}>
@@ -55,7 +107,7 @@ export default function Ayuda() {
                 style={{ marginLeft: "5px" }}
               >
                 <Button
-                  form="editActividad"
+                  form="solicitudAyuda"
                   type="submit"
                   className="btnn-primary"
                 >
