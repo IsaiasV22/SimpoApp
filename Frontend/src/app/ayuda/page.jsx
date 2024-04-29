a5a126eb8b45d5b09ca5a235cdd99fe6067fce08
IsaiasViquez <viquez.isaias@gmail.com>
2024-04-28 19:29:21 -0600

    Navegación

---
+++ Frontend/src/app/ayuda/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import Calendar from "@/app/components/ayuda/Ayuda";
+
+export async function Page() {
+  return (
+    <div>
+        <h1>Help</h1>
+        <p>
+            This is the help page. Here you can find information about how to use
+            the application.
+        </p>
+    </div>
+  );
+}
+
+export default Page;
---
+++ Frontend/src/app/solicitudesAyuda/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+//import SolicitudesAyuda from "@/app/components/solicitudesAyuda/SolicitudesAyuda";
+
+export async function Page() {
+  return (
+    <div>
+        <h1>
+            Solicitudes ayuda
+        </h1>
+    </div>
+  );
+}
+
+export default Page;
