80a4050002972b5823e794a411590b946da72f38
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2024-05-01 22:29:10 -0600

    Recordatorio persiste

--- Backend/src/controllers/recordatoriosController.js
+++ Backend/src/controllers/recordatoriosController.js
@@ -1,12 +1,22 @@
 const db = require("../../config/database.js");
+const { promisify } = require("util");
+//pomisify will convert db.query into a promise
+const dbQuery = promisify(db.query).bind(db);
 
 // Obtener todos los recordatorios
 //fetch to mock server for now
 exports.getRecordatorios = async (req, res) => {
   try {
-    const recordatorios = await fetch('https://89b173d5-53df-4b1c-8041-72f768f0c508.mock.pstmn.io/recordatorios');
-    const data = await recordatorios.json();
-    res.status(200).json(data);
+    //get from database
+    const id = req.params.id;
+    console.log("id:", id);
+    //SELECT * FROM RECORDATORIO WHERE FK_ACTIVIDAD = 1;
+    const results = await dbQuery(
+      `SELECT * FROM recordatorio WHERE FK_ACTIVIDAD = ${id}`
+    );
+    if (results.length == 0)
+      res.status(204).json({ message: "No se encontraron recordatorios" });
+    res.status(200).json(results);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Error al obtener recordatorios" });
@@ -14,20 +24,22 @@ exports.getRecordatorios = async (req, res) => {
 };
 
 exports.postRecordatorio = async (req, res) => {
-    const { recordatorio } = req.body;
-    try {
-        const response = await fetch('https://89b173d5-53df-4b1c-8041-72f768f0c508.mock.pstmn.io/recordatorios', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ recordatorio }),
-        });
-        if (response.status === 204) {
-        res.status(204).json({ message: "Recordatorio guardado correctamente" });
-        }
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Error al guardar recordatorio" });
+  try {
+    console.log('body:', req.body)
+    const recordatorio = req.body.recordatorio;
+    const actividadId = req.body.id;
+    //insert into database
+    //INSERT INTO recordatorio (recordatorio, FK_ACTIVIDAD) VALUES ('recordatorio', 1);
+    const response = await dbQuery(
+      `INSERT INTO recordatorio (recordatorio, FK_ACTIVIDAD) VALUES ('${recordatorio}', ${actividadId})`
+    );
+
+    console.log("values:", recordatorio, actividadId);
+    if (response.affectedRows > 0) {
+      res.status(204).json({ message: "Recordatorio guardado correctamente" });
     }
-    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error al guardar recordatorio" });
+  }
+};
