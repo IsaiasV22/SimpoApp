633c9678f4c3b63f4204670f708e7473c312757c
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2024-05-03 20:48:42 -0600

    Cache strategy
    stale while revalidate

--- Frontend/public/sw.js
+++ Frontend/public/sw.js
@@ -1,5 +1,8 @@
 console.log("Service worker loaded");
 
+//cache name
+const cacheName = "simpoapp_cache_v2_Cache_then_Network";
+
 //intercept any push notification and show it
 self.addEventListener("push", function (event) {
   console.log("Push notification intercepted by sw -> ", event);
@@ -18,5 +21,20 @@ self.addEventListener("push", function (event) {
 
 //intercept any fetch request and console log the url
 self.addEventListener("fetch", function (event) {
-  console.log("Fetch intercepted by sw -> ", event.request.url);
+  //stale while revalidate
+  /*
+  This strategy returns cached data while asynchronously revalidating it in the background.
+  Provides both fast response times with cached data and ensures the cache is updated with the latest data.
+  */
+  event.respondWith(
+    caches.open(cacheName).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        var fetchPromise = fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    })
+  );
 });
